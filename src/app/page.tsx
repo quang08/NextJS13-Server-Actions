97d23943965d15b9d2dbd0fab6c8feba3dbe5f83
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-05-06 20:32:28 +0700

    set up server actions for newsletter form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+
 export default function Home() {
+  //server action
+  async function myFormProcessor(formData: any) {
+    "use server";
+    console.log(formData);
+  }
   return (
     <main className="h-screen w-screen flex justify-center items-center">
       <div className="h-full w-1/2 md:max-w-3/4 flex flex-col justify-center items-center gap-4">
         <h1 className="text-4xl font-bold">Newsletter</h1>
         <form
+          action={myFormProcessor}
           autoComplete="off"
           className="flex flex-col w-3/4 md:max-w-lg gap-6"
         >
