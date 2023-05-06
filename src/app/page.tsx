be06cd26cbe28912ad778802499a25e56b7fe321
Quang Nguyen The <quangnguyenthe@Quangs-MacBook-Air.local>
2023-05-06 19:44:35 +0700

    server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-'use client'
+//what if we dont't want to declare this as a client component, but still want to keep the interactivity for the server-side 
+//like making a DB request?
 
 export default function Home() {
+  //we define a server-side function (has to be async)
+  async function consoleLog() {
+    "use server" //only be run on the server and we wont see the log in the inspector since it's sever-side
+    console.log("Hello");
+  }
+
   return (
     <div className="bg-black h-screen w-screen">
-      <button onClick={() => console.log("Hello")}>Log message</button>
+      <form action={consoleLog}>
+        <button>console log</button>
+      </form>
     </div>
   );
 }
